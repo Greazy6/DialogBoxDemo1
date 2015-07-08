20cec5419a4b81341d8cf93e4828060489e984ff
Greazy6 <grclkrc@hotmail.com>
2015-07-08 14:01:39 -0400

    JOptionPane demo

---
+++ src/dialogboxdemo1/DialogBoxDemo1.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dialogboxdemo1;
+
+import javax.swing.JOptionPane;
+
+
+public class DialogBoxDemo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String name = JOptionPane.showInputDialog(null, "Please enter your first and last name");
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+        
+        String nName = JOptionPane.showInputDialog(null, "Did you like seing your name? Yes or No");
+        JOptionPane.showMessageDialog(null,name + " Selected " + nName);
+    }
+    
+}
